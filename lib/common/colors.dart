694beb39395dbe824d128fb2a6f00c0196cc59cc
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-02-05 23:47:40 +0900

    add common color definitions

---
+++ lib/common/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color PrimaryColor = const Color(0xFF7579F6);
+
+const Color SelectedButtonBorder = const Color(0xFF3338D0);
+const Color UnselectedButtonBorder = const Color(0xFFE6E6E6);
+
+const Color SelectedButtonText = Colors.white;
+const Color UnselectedButtonText = const Color(0xFFA6A6A6);
+
+const Color SelectedButton = PrimaryColor;
+const Color UnselectedButton = const Color(0xFFF9F9F9);
