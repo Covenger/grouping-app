46a2fc58bc0ea8fe3e5d5b91dd0dab440b8a8563
Byeonghoon Yoo <bh322yoo@gmail.com>
2019-08-19 22:47:48 +0900

    Narrowing type of SVG images

--- lib/common/icons.dart
+++ lib/common/icons.dart
@@ -1,42 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-final Widget passwordIcon = SvgPicture.asset(
+final SvgPicture passwordIcon = SvgPicture.asset(
   'assets/svg/password_icon.svg',
   semanticsLabel: 'Password Icon',
 );
 
-final Widget emailIcon = SvgPicture.asset(
+final SvgPicture emailIcon = SvgPicture.asset(
   'assets/svg/email_icon.svg',
   semanticsLabel: 'Email Icon',
 );
 
-final Widget bellIcon = SvgPicture.asset(
+final SvgPicture bellIcon = SvgPicture.asset(
   'assets/svg/bell_icon.svg',
   semanticsLabel: 'Notification Icon',
 );
 
-final Widget feedIcon = SvgPicture.asset(
+final SvgPicture feedIcon = SvgPicture.asset(
   'assets/svg/feed_btn.svg',
   semanticsLabel: 'Feed button Icon',
 );
 
-final Widget groupIcon = SvgPicture.asset(
+final SvgPicture groupIcon = SvgPicture.asset(
   'assets/svg/group_btn.svg',
   semanticsLabel: 'Group button Icon',
 );
 
-final Widget recommendIcon = SvgPicture.asset(
+final SvgPicture recommendIcon = SvgPicture.asset(
   'assets/svg/recom_btn.svg',
   semanticsLabel: 'Recommendation button Icon',
 );
 
-final Widget searchIcon = SvgPicture.asset(
+final SvgPicture searchIcon = SvgPicture.asset(
   'assets/svg/search_icon.svg',
   semanticsLabel: 'Search button Icon',
 );
 
-final Widget settingIcon = SvgPicture.asset(
+final SvgPicture settingIcon = SvgPicture.asset(
   'assets/svg/setting_btn.svg',
   semanticsLabel: 'Setting button Icon',
 );
